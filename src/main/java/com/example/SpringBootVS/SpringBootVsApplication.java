28d251f68a42f3f10d46887c6bf6990c06b6e707
omkarchorghe16 <omkarchorghe16@gmail.com>
2023-11-07 00:20:42 -0500

    first commit

---
+++ src/main/java/com/example/SpringBootVS/SpringBootVsApplication.java
@@ -0,0 +1,13 @@
+package com.example.SpringBootVS;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootVsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootVsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/SpringBootVS/SpringBootVsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.SpringBootVS;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootVsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
